aeacdb448d39408661158cd0085a8aab23bc8a16
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-08-17 13:17:34 +0200

    init + tp01 en cours

---
+++ js/01.function.js
@@ -0,0 +1,12 @@
+
+//console.log("01 - Fonctions");
+
+let nombre1 = 10;
+let nombre2 = 20;
+
+function additionner(nb1, nb2) {
+    return nb1 + nb2;
+}
+
+let resultat1 = additionner(nombre1, nombre2);
+console.log("resultat1 :", resultat1);
