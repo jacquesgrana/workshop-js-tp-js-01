ec0ee0a9a475706fe810ec5125b0f2f70f6dbb9c
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-08-17 13:44:11 +0200

    tp01 ok

--- js/01.function.js
+++ js/01.function.js
@@ -10,3 +10,32 @@ function additionner(nb1, nb2) {
 
 let resultat1 = additionner(nombre1, nombre2);
 console.log("resultat1 :", resultat1);
+
+let somme = additionner;
+
+let resultat2 = somme(nombre1, nombre2);
+console.log("resultat2 :", resultat2);
+
+let multiplication = function(nb1, nb2) {
+    return nb1*nb2;
+};
+
+let soustraction = function(nb1, nb2) {
+    return nb1 - nb2;
+}
+
+let resultat3 = multiplication(nombre1, nombre2);
+console.log("resultat3 :", resultat3);
+
+let afficherOperation = function(nomOperation, operation, nb1, nb2) {
+    let toDisplay = nomOperation + "(" + nb1 + ", " + nb2 + ") = ";
+
+    toDisplay += operation(nb1, nb2);
+    //toDisplay += somme(nb1, nb2) + ">";
+
+    console.log(toDisplay);
+};
+
+afficherOperation("addition", somme, nombre1, nombre2);
+afficherOperation("multiplication", multiplication, nombre1, nombre2);
+afficherOperation("soustraction", soustraction, 15, 5);
